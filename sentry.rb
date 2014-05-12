f4ddd7530d928bd96b473c99b561e60cc77b4408
SixArm <sixarm@sixarm.com>
2014-05-11 20:08:07 -0600

    Upgrade to optparse

--- sentry.rb
+++ sentry.rb
@@ -9,12 +9,18 @@
 module Sentry
 end
 
+## Options
+require 'optparse'
+require 'ostruct'
+
+## Networking
 require 'net/http'
 require 'net/smtp'
 require 'socket'
 require 'resolv'
 require 'resolv-replace'
 
+## Sentry
 require_relative 'sentry/extensions/array'
 require_relative 'sentry/help'
 require_relative 'sentry/helpers/http'
@@ -27,54 +33,143 @@ require_relative 'sentry/watchers/watch_dns'
 require_relative 'sentry/watchers/watch_uri'
 
 
-#### ARGS ####################################################################
+#### OPTS ####################################################################
 
-args = ARGV.to_h
+options = OpenStruct.new
 
-dns          = args['--dns']
-uri          = args['--uri']
+optparse = OptionParser.new do |opts|
 
-n            = (args['-n'] || args['--number'] || 1).to_i
-speed        = (args['-s'] || args['--speed'] || 0.00).to_f
+  opts.banner = "Usage: sentry [options]"
 
-include_text = args['-i'] || args['--include']
-exclude_text = args['-e'] || args['--exclude']
+  ## Help
 
-mail_flag    = args['--mail']         || args['--mail-to'] || args['--mail-to'] || args['--mail-to']
-mail_to      = args['--mail-to']      || mail_to_default
-mail_from    = args['--mail-from']    || mail_from_default
-mail_subject = args['--mail-subject'] || mail_subject_default
+  opts.on('-h', '--help', 'Help') do
+    help
+    puts opts
+    exit
+  end
 
-user_name    = args['-u'] || args['--user'] || user_name_default
-host_name    = args['-h'] || args['--host'] || host_name_default
+  ## Watchers
+
+  opts.on('--dns [host name]', 'Test that the system can resolve a DNS host name') do |x|
+    options.dns = x
+  end
+
+  opts.on('--uri [uri text]', 'Test that the system can fetch URI') do |x|
+    p "x:#{x}"
+    options.uri = x
+    p "options.uri:#{options.uri}"
+  end
+
+  ## Diagnostics
+
+  opts.on('-n', '--number [number of times to run]', 'Run the test _n_ times') do |x|
+    options.n = x
+  end
+
+  opts.on('-s', '--speed [seconds]', 'Verify the test runs within _s_ seconds') do |x|
+    options.speed = x
+  end
+
+  ## Text OK?
+
+  opts.on('-i', '--include [text]', 'Verify the result must include text') do |x|
+    options.include_text = x
+  end
+
+  opts.on('-e', '--exclude [text]', 'Verify the result must exclude text') do |x|
+    options.exclude_text = x
+  end
+
+  ## Mail
+
+  opts.on('--mail', 'Send alert via mail, using default settings') do
+    options.mail_flag = true
+  end
+
+  opts.on('--mail-to [email address]', 'Send alert via mail, to a given email address') do |x|
+    options.mail_to = x
+  end
+
+  opts.on('--mail-from [email address]', 'Send alert via mail, from a given email address') do |x|
+    options.mail_from = x
+  end
+
+  opts.on('--mail-subject [text]', 'Send alert via mail, using a given email subject') do |x|
+    options.mail_subject = x
+  end
+
+  ## System
+
+  opts.on('--user [user name]', 'The local system user name') do |x|
+    options.user_name = x
+  end
+
+  opts.on('--host [host name]', 'The local system host name') do |x|
+    options.host_name = x
+  end
+
+end
+
+optparse.parse!
 
 
 #### MAIN ####################################################################
 
-if !(dns || uri)
+if !(options.dns || options.uri)
   help
   exit -1
 end
 
+watch = nil
+p options
+p ARGV
+
 begin
+
+puts "here"
+
+  # Create
   watch = \
-    if dns
-      Sentry::WatchDNS.new(dns)
-    elsif uri
-      Sentry::WatchURI.new(URI.parse(uri))
+    if options.dns
+      p "dns"
+      Sentry::WatchDNS.new(options.dns)
+    elsif options.uri
+      x = options.uri
+      p x
+      x.index('://') or x = "http://#{x}"
+      p x
+      uri = URI.parse(x)
+      p "uri:#{uri}"
+      Sentry::WatchURI.new(uri)
     else
       raise
     end
+
+  puts "here2"
+  puts watch.name
+
+  ## Run
+  n = options.n || 1
   secs = speedtest { n.times { watch.run } } / n
-  message = "#{secs} secs average, #{n} number of times, #{uri}"
-  if speed > 0 and secs > speed then raise "Too slow: #{secs}>#{speed}" end
-  text = watch.text
-  if include_text && !text.index(include_text) then raise "Failed include: #{include_text}" end
-  if exclude_text &&  text.index(exclude_text) then raise "Failed exclude: #{exclude_text}" end
+  message = "Sentry #{watch.name} => speed:#{secs}s" + ((n==1) ? "" : " (avg, n=#{n})")
+
+  ## Speed OK?
+  x = options.speed; if secs > x then raise "Too slow: #{secs}>#{x}" end
+
+  ## Text OK?
+  x = options.include_text; if x && !watch.text.index(x) then raise "Failed include: #{x}" end
+  x = options.exclude_text; if x &&  watch.text.index(x) then raise "Failed exclude: #{x}" end
+
+  ## Results
   puts message
+
 rescue
-  puts vitals = [uri,$!,message,`date`,`uname -a`,`w`,`ps -ef`] * "\n\n"
+  puts vitals = [watch.name, $!, message, `date`, `uname -a`, `w`, `ps -ef`] * "\n\n"
   if mail_flag
-    send_message_with_headers(vitals, mail_from, mail_to, mail_subject)
+    from     = options.mail_from     || mail_from_default
+    to       = options.mail_to       || mail_to_default
+    subject  = options.mail_subject  || mail_subject_default
+    send_message_with_headers(vitals, from, to, subject)
   end
 end
